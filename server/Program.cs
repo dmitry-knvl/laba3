0f80b23d2f9dc391a9b48fa7b1876bc6507b6166
deutschbomb <lesha.levitskiy@mail.ru>
2023-04-16 18:03:03 +0300

    Перепись серверения

--- server/Program.cs
+++ server/Program.cs
@@ -1,16 +1,56 @@
-﻿using Serverito;
+﻿using System;
+using System.IO;
+using System.Net;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Директория, которую мы будем отображать
+        string directory = @"../client";
 
-namespace ЛабРаб3 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			ServeritoListener server = new ServeritoListener("http://localhost:8000/");
-			server.AddView(new URL("/"), (ServeritoContext context) =>
-			{
-				Utils.WriteToResponse(context.Context, "Hello World!");
-			});
-			server.Start();
-		}
-	}
+        // Создаем экземпляр HttpListener
+        var listener = new HttpListener();
+
+        // Добавляем префикс, по которому будем слушать запросы
+        listener.Prefixes.Add("http://localhost:8080/");
+    
+    // Начинаем прослушивание запросов
+        listener.Start();
+
+        Console.WriteLine("Server started");
+
+        while (true)
+        {
+            // Ждем входящий запрос
+            var context = listener.GetContext();
+
+            // Получаем запрашиваемый URL
+            string url = context.Request.Url.LocalPath;
+
+            // Собираем полный путь к файлу
+            string path = directory + url;
+
+            // Если файл существует, отправляем его содержимое в ответ
+            if (File.Exists(path))
+            {
+                context.Response.ContentType = "application/octet-stream";
+                context.Response.ContentLength64 = new FileInfo(path).Length;
+                context.Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(path));
+
+                using (var stream = File.OpenRead(path))
+                {
+                    stream.CopyTo(context.Response.OutputStream);
+                }
+            }
+            else
+            {
+                // Если файл не найден, отправляем 404 ошибку
+                context.Response.StatusCode = 404;
+                context.Response.Close();
+            }
+        }
+    }
 }
+
+
+
