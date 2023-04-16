8d7c9066cded8d40c326117ba905edcd3dbb36bc
deutschbomb <lesha.levitskiy@mail.ru>
2023-04-16 17:21:48 +0300

    Содзание севрера

---
+++ server/Program.cs
@@ -0,0 +1,16 @@
+﻿using Serverito;
+
+namespace ЛабРаб3 {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServeritoListener server = new ServeritoListener("http://localhost:8000/");
+			server.AddView(new URL("/"), (ServeritoContext context) =>
+			{
+				Utils.WriteToResponse(context.Context, "Hello World!");
+			});
+			server.Start();
+		}
+	}
+}
